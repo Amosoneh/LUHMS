5dfc3a8d537d53c343fa7fa088def4ec5f964c7f
MichaelBoyo <boyomichaelbidemi@gmail.com>
2022-09-06 16:39:54 +0100

    second commit

---
+++ src/main/java/com/luminaries/LUHMS.java
@@ -0,0 +1,11 @@
+package com.luminaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LUHMS {
+    public static void main(String[] args) {
+        SpringApplication.run(LUHMS.class,args);
+    }
+}
